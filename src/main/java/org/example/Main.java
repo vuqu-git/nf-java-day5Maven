7bd4f562d9fd01226a2e997e80e019508c230493
vuqu-git <vuqu@fantasymail.de>
2025-02-06 16:53:05 +0100

    added FigleFont usage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
+import com.github.lalyos.jfiglet.FigletFont;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.print("Hello and welcome!");
         System.out.print("2nd output here");
+
+        String asciiArt = FigletFont.convertOneLine("alte fische!");
+        System.out.println(asciiArt);
+
 
     }
 }
